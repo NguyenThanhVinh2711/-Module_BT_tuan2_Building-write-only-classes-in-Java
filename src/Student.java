27df53272c932f9ab058446270e80cce56218176
boomka <vinh99657@gmail.com>
2022-08-10 14:28:00 +0700

    tuan2

---
+++ src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private String name = "john";
+    private String Classes = "C02";
+    Student(){
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return Classes;
+    }
+
+    public void setClasses(String classes) {
+        Classes = classes;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Student test = new Student();
+        test.setName("vinh");
+        test.setClasses("C06");
+        System.out.println(test.getName() + "\n");
+        System.out.println(test.getClasses());
+    }
+}
